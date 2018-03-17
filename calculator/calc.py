0ded301f1b2edec78223c3f41f60663db16970fe
Raragyay <let987let987@gmail.com>
2018-03-17 16:51:26 -0400

    Calculator

--- calculator/calc.py
+++ calculator/calc.py
@@ -2,13 +2,13 @@ import tkinter as tk
 from collections import deque
 
 calc = tk.Tk()
-calc.title('Taken from Moonie. <3')
+calc.title('Many Thanks to Moonie for telling me how to create buttons. <3')
 
 buttons = [
-    ['7', '8', '9', '*', 'CA', '('],
-    ['4', '5', '6', '/', 'C', ')'],
-    ['1', '2', '3', '-', 'SHOW', 'WASTE'],
-    ['+/-', '0', '.', '+', '=', 'WASTE']
+    ['7', '8', '9', '*', '<-', '('],
+    ['4', '5', '6', '/', 'MOD', ')'],
+    ['1', '2', '3', '-', '^', 'CA'],
+    ['+/-', '0', '.', '+', '=', 'C']
 ]
 button_style = 'raised'
 for fake_row_num, row in enumerate(buttons):
@@ -21,46 +21,48 @@ for fake_row_num, row in enumerate(buttons):
 display = tk.Entry(calc, width=40, bg='white', state='readonly', justify='right')
 display.grid(row=0, column=0, columnspan=5)
 stack = deque()
-operators = {'*', '/', '-', '+'}
 precedences = {'+': 2, '-': 2, '*': 3, '/': 3}
+clear_next = False
 operations = {
-    '+': lambda x, y: x + y,
-    '-': lambda x, y: x - y,
-    '*': lambda x, y: x * y,
-    '/': lambda x, y: x / y
+    '+'  : lambda x, y: x + y,
+    '-'  : lambda x, y: x - y,
+    '*'  : lambda x, y: x * y,
+    '/'  : lambda x, y: x / y,
+    '^'  : lambda x, y: x ** y,
+    'MOD': lambda x, y: x % y
 }
 
 
 def calc_expr():
     expression = stack.copy()
-    operands = []
-    operators = []
+    operand_stack = []
+    operator_stack = []
     while expression:
         token = expression.popleft()
         if type(token) == float:
-            operands.append(token)
+            operand_stack.append(token)
         elif token == '(':
-            operators.append(token)
+            operator_stack.append(token)
         elif token == ')':  # TODO: Add parentheses
-            while operators[-1] != '(':
-                operator = operators.pop()
-                operand_2 = operands.pop()
-                operand_1 = operands.pop()
-                operands.append(operations[operator](operand_1, operand_2))
-            operators.pop()
+            while operator_stack[-1] != '(':
+                operator = operator_stack.pop()
+                operand_2 = operand_stack.pop()
+                operand_1 = operand_stack.pop()
+                operand_stack.append(operations[operator](operand_1, operand_2))
+            operator_stack.pop()
         else:
-            while operators and precedences[operators[-1]] >= precedences[token]:
-                operator = operators.pop()
-                operand_2 = operands.pop()
-                operand_1 = operands.pop()
-                operands.append(operations[operator](operand_1, operand_2))
-            operators.append(token)
-    while operators:
-        operator = operators.pop()
-        operand_2 = operands.pop()
-        operand_1 = operands.pop()
-        operands.append(operations[operator](operand_1, operand_2))
-    result = sum(operands)  # By this point there should only be one operand: the result.
+            while operator_stack and precedences[operator_stack[-1]] >= precedences[token]:
+                operator = operator_stack.pop()
+                operand_2 = operand_stack.pop()
+                operand_1 = operand_stack.pop()
+                operand_stack.append(operations[operator](operand_1, operand_2))
+            operator_stack.append(token)
+    while operator_stack:
+        operator = operator_stack.pop()
+        operand_2 = operand_stack.pop()
+        operand_1 = operand_stack.pop()
+        operand_stack.append(operations[operator](operand_1, operand_2))
+    result = sum(operand_stack)  # By this point there should only be one operand: the result.
     if int(result) == result:
         return int(result)
     else:
@@ -81,7 +83,7 @@ def click_event(button):
     if button == '.':
         if '.' not in display.get():
             display.insert(tk.END, '.')
-    elif button in operators or button in {'(', ')'}:
+    elif button in operations or button in {'(', ')'}:
         num = float(display.get())
         stack.append(num)
         stack.append(button)
@@ -92,10 +94,12 @@ def click_event(button):
         else:
             display.insert(0, '-')
     elif button == 'CA':
-        stack = []
+        stack = deque()
         display.delete(0, tk.END)
     elif button == 'C':
         display.delete(0, tk.END)
+    elif button == '<-':
+        display.delete(len(display.get()) - 1)
     elif button == '=':
         num = float(display.get())
         stack.append(num)
